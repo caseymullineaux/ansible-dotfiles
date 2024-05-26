9e5dcc07b5eca65c3ff411c54297829d252e3fdf
Casey Mullineaux <casey@mullineaux.com.au>
2024-05-26 21:49:24 +1000

    batman

---
+++ roles/bash/files/bash/paths_functions.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+addToPath() {
+    if [[ "$PATH" != *"$1"* ]]; then
+        export PATH=$PATH:$1
+    fi
+}
+
+addToPathFront() {
+    if [[ "$PATH" != *"$1"* ]]; then
+        export PATH=$1:$PATH
+    fi
+}
+
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
---
+++ roles/bash/files/bash/paths_vars.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+addToPath $HOME/.dotfiles/bin
+addToPathFront $HOME/.local/bin
